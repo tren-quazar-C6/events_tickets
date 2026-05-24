ee7072248d0befabbda89125e1867c3175989081
Veronica Martinez Cadavid <veromar.c96@gmail.com>
2026-05-24 14:48:24 -0500

    fix: VPS mongo routing

--- Configuration/MongoLoggingOptions.cs
+++ Configuration/MongoLoggingOptions.cs
@@ -2,11 +2,11 @@ namespace events_tickets.Configuration;
 
 public sealed class MongoLoggingOptions
 {
-    public const string SectionName = "MongoLogging";
+    public const string SectionName = "MongoDB"; 
 
     public string ConnectionString { get; init; } = "mongodb://localhost:27017";
 
-    public string DatabaseName { get; init; } = "events_observability";
+    public string DatabaseName { get; init; } = "events_logs"; 
 
     public string SalesLogsCollection { get; init; } = "sales_logs";
 
